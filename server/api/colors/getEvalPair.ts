3a58fcd3cef4e519b1b038c6af06db9c61abf065
piman51277 <piman.dev@gmail.com>
2023-09-28 20:05:15 -0400

    guarentee that color pairs are different

--- server/api/colors/getEvalPair.ts
+++ server/api/colors/getEvalPair.ts
@@ -6,7 +6,12 @@ const schema = Joi.object({});
 
 export default handler(schema, async (req, res) => {
   const leftColor = generateColor();
-  const rightColor = generateColor();
+  let rightColor = "";
+
+  //having duplicate colors is pointless
+  while (rightColor === "" || rightColor === leftColor) {
+    rightColor = generateColor();
+  }
 
   res.json({
     leftColor,
