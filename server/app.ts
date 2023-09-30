e1012c496be7cf162035f29a0749a7a71d863d10
piman51277 <piman.dev@gmail.com>
2023-09-29 22:25:18 -0400

    add CORS & CSP headers for sec

--- server/app.ts
+++ server/app.ts
@@ -5,12 +5,23 @@ import express from "express";
 
 export const app = express();
 
+//set CORS & CSP headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://colors.piman.dev");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Content-Security-Policy", "default-src 'self'");
+  next();
+});
+
 //static assets
 const oneWeek = 1000 * 60 * 60 * 24 * 7;
 
 const cacheTime = process.env.ENVIROMENT === "dev" ? 0 : oneWeek;
 
-app.use("/asset", express.static("./web/assets", { maxAge: cacheTime }));
+app.use(
+  "/asset",
+  express.static("./web/assets", { maxAge: cacheTime, immutable: true })
+);
 
 //body parser
 import bodyParser from "body-parser";
